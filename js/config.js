b56387c9f5e4edfdc8de661df12a8bc738b27bf9
fred <runtesting4me@gmail.com>
2025-01-25 15:59:54 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_f9ugaemfZ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6akipgkrvfaqd4k2ivpurphcpe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
